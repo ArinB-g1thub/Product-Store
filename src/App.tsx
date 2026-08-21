362abbbd06a9097550c5c6d649ef7277d543ad7c
ArinB-g1thub <arinbaral20@gmail.com>
2026-08-21 09:07:38 +0545

    React Project: Product-Store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Button } from "@/components/ui/button"
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Cart from "./pages/Cart"
+import Home from "./pages/Home"
+import ProductDetail from "./pages/ProductDetail"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <Navbar/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/ProductDetail/:id" element={<ProductDetail/>}/>
+          <Route path="/Cart" element={<Cart/>}/>
+          <Route path="*" element={<p>Page Not Found</p>}/>
+        </Routes>
+      </main>
     </div>
   )
 }
 
-export default App
+export default App;
